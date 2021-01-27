27cae3f10e0387014f4979572face98ee53c3975
Aleksei Terentev <aleksei.terentev@NetCracker.com>
2021-01-28 00:56:11 +0400

    lesson5

---
+++ lesson5/src/main/java/ru/geekbrains/terentev/task02/Task02Employee.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.terentev.task02;
+
+public class Task02Employee extends ru.geekbrains.terentev.task01.Employee {
+
+    public Task02Employee(String fio, String position, String email, String phone, Integer salary, Integer age) {
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+}
